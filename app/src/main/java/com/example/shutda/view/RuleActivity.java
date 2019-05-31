a5775e070dcfc04c2c4cec986d3a20e1f33e8260
Inviea <jungle0220@gmail.com>
2019-06-01 07:28:56 +0900

    rank cardrank 제외하고 조금 더 수정해봄...

--- app/src/main/java/com/example/shutda/view/RuleActivity.java
+++ app/src/main/java/com/example/shutda/view/RuleActivity.java
@@ -3,16 +3,17 @@ package com.example.shutda.view;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.example.shutda.R;
 
 public class RuleActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rule);
-
         final View decorView = getWindow().getDecorView();
         final int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
